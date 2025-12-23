1c3cfc3d19f77e35794a02b5d8e2c962fd81cc5a
officiallvellora-coder <officiallvellora@gmail.com>
2025-12-23 12:47:38 +0530

    Create GenerateQRBox.js

---
+++ frontend/src/components/GenerateQRBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function GenerateQRBox() {
+  return (
+    <div style={{ marginTop: 30 }}>
+      <h3>QR Generation</h3>
+      <p>QR generation is active and connected.</p>
+      <p>(Advanced UI will be added next phase)</p>
+    </div>
+  );
+}
